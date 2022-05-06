ff17ab641b09df8a96b46ed174479f763e7e264a
rupert-ma <michael.a.rupert@gmail.com>
2022-05-06 13:16:36 -0700

    refactored main logic into its own function

--- daytripgenerator.py
+++ daytripgenerator.py
@@ -21,69 +21,77 @@ def generate_transportation():
 def generate_entertainment():
     return random.choice(entertainment)
 
+#confirm selections or reslect, print finalized trip, 
+def confirm_generated_selections(destination, restaurant, mode_of_travel, recreation):
+    trip_confirmed = False
+    while trip_confirmed == False:
+        
+        destination_confirm = 'n'
+        while destination_confirm == 'n':
+            destination_confirm = input(f'We selected {destination} for you, do you accept? y/n ')
+            if destination_confirm == 'n':
+                destinations.remove(destination)
+                destination = generate_destination()
+            elif destination_confirm == 'y':
+                print(f'Great {destination} it is')
+                destination_confirm = 'y'
+
+        restaurant_confirm = 'n'
+        while restaurant_confirm == 'n':
+            restaurant_confirm = input(f'We selected {restaurant} for you, do you accept? y/n ')
+            if restaurant_confirm == 'n':
+                restaurants.remove(restaurant)
+                restaurant = generate_restaurant()
+            elif restaurant_confirm == 'y':
+                print(f'Great {restaurant} it is')
+                restaurant_confirm = 'y'
+        
+        transportation_confirm = 'n'
+        while transportation_confirm == 'n':
+            transportation_confirm = input(f'We selected {mode_of_travel} for you, do you accept? y/n ')
+            if transportation_confirm == 'n':
+                transportation.remove(mode_of_travel)
+                mode_of_travel = generate_transportation()
+            elif transportation_confirm == 'y':
+                print(f'Great {mode_of_travel} it is')
+                transportation_confirm = 'y'
+
+        entertainment_confirm = 'n'
+        while entertainment_confirm == 'n':
+            entertainment_confirm = input(f'We selected {recreation} for you, do you accept? y/n ')
+            if entertainment_confirm == 'n':
+                entertainment.remove(recreation)
+                recreation = generate_entertainment()
+            elif entertainment_confirm == 'y':
+                print(f'Great {recreation} it is')
+                entertainment_confirm = 'y'
+
+        if destination_confirm == 'y' and restaurant_confirm == 'y' and transportation_confirm == 'y' and entertainment_confirm == 'y':
+            trip_confirmed = True
+
+    print('The trip generated for you is: ')
+    print(f'Destination: {destination}')
+    print(f'Transportation: {mode_of_travel}')
+    print(f'Restaraunt: {restaurant}')
+    print(f'Entertainment: {recreation}')
+
+#calls functions to generate options and store values
 destination = generate_destination()
 restaurant = generate_restaurant()
 mode_of_travel = generate_transportation()
 recreation = generate_entertainment()
 
-#confirm selections or reslect, print finalized trip, 
-trip_confirmed = False
-while trip_confirmed == False:
-    
-    destination_confirm = 'n'
-    while destination_confirm == 'n':
-        destination_confirm = input(f'We selected {destination} for you, do you accept? y/n ')
-        if destination_confirm == 'n':
-            destinations.remove(destination)
-            destination = generate_destination()
-        elif destination_confirm == 'y':
-            print(f'Great {destination} it is')
-            destination_confirm = 'y'
-    
-    restaurant_confirm = 'n'
-    while restaurant_confirm == 'n':
-        restaurant_confirm = input(f'We selected {restaurant} for you, do you accept? y/n ')
-        if restaurant_confirm == 'n':
-            restaurants.remove(restaurant)
-            restaurant = generate_restaurant()
-        elif restaurant_confirm == 'y':
-            print(f'Great {restaurant} it is')
-            restaurant_confirm = 'y'
-    
-    transportation_confirm = 'n'
-    while transportation_confirm == 'n':
-        transportation_confirm = input(f'We selected {mode_of_travel} for you, do you accept? y/n ')
-        if transportation_confirm == 'n':
-            transportation.remove(mode_of_travel)
-            mode_of_travel = generate_transportation()
-        elif transportation_confirm == 'y':
-            print(f'Great {mode_of_travel} it is')
-            transportation_confirm = 'y'
+#calls function to confirm previously generated trip options
+confirm_generated_selections(destination, restaurant, mode_of_travel, recreation)
 
-    entertainment_confirm = 'n'
-    while entertainment_confirm == 'n':
-        entertainment_confirm = input(f'We selected {recreation} for you, do you accept? y/n ')
-        if entertainment_confirm == 'n':
-            entertainment.remove(recreation)
-            recreation = generate_entertainment()
-        elif entertainment_confirm == 'y':
-            print(f'Great {recreation} it is')
-            entertainment_confirm = 'y'
-
-    if destination_confirm == 'y' and restaurant_confirm == 'y' and transportation_confirm == 'y' and entertainment_confirm == 'y':
-        trip_confirmed = True
-
-print('The trip generated for you is: ')
-print(f'Destination: {destination}')
-print(f'Transportation: {mode_of_travel}')
-print(f'Restaraunt: {restaurant}')
-print(f'Entertainment: {recreation}')
-
-
+#finalizes trip 
 confirm_final = input('Would you like to finalize this trip? y/n ')
 if confirm_final == 'n':
-    print('Ok rerun the program and we will start over')
-
+    startover = input('Ok do you want to start over? y/n ')
+    if startover =='y':
+        confirm_generated_selections(destination, restaurant, mode_of_travel, recreation)
+    else:
+        print('Ok, exiting program. Please try again later')
 else:
     print(f'Your completed trip is to {destination} with dinner at {restaurant}, transportation will be {mode_of_travel} and {recreation} for entertainment.')
 
